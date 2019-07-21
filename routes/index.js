18f4f00ebd4a21bb835dc1ba735c78b9480b3282
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-07-21 17:15:47 +0530

    Added services

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get("/about", (req, res) => {
   res.render("about", { title: "About | Edushala" });
 });
 
+router.get("/services", (req, res) => {
+  res.render("services", { title: "Services | Edushala" });
+});
+
 module.exports = router;
